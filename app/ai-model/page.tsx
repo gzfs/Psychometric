85f422c8741946451d08a07347d70c3def5eae14
Vishal <vismbs@pm.me>
2025-01-06 14:56:51 +0530

    feat: fixed

--- app/ai-model/page.tsx
+++ app/ai-model/page.tsx
@@ -1,17 +1,8 @@
 "use client";
 
-import ModelResultsPage from "@/components/ai-model-extra";
 import AIModelTest from "@/components/ai-models-test";
-import { Switch } from "@/components/ui/switch";
 import { useState } from "react";
 
 export default function AIModelTestPage() {
-  const [isTraditional, setIsTraditional] = useState(true);
-  return (
-    <div className="flex flex-col items-center py-5">
-      <Switch onCheckedChange={(checked) => setIsTraditional(checked)} />
-      {isTraditional && <AIModelTest />}
-      {!isTraditional && <ModelResultsPage />}
-    </div>
-  );
+  return <AIModelTest />;
 }
